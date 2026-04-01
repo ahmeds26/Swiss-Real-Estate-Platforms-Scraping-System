18e3b5b44a2fff445deb112d8d23f0e7acc17f43
ahmeds26 <ahmed.saeeed@outlook.com>
2026-04-01 11:53:26 +0200

    pushing the scraper for icasa platform

---
+++ gregor_repo/headers.py
@@ -0,0 +1,70 @@
+
+
+icase_search_headers = [
+        "category", 
+        "url", 
+        "image", 
+        "price", 
+        "price_value", 
+        "address", 
+        "locality", 
+        "region", 
+        "postal_code", 
+        "country", 
+        "latitude", 
+        "longitude", 
+        "rooms_number", 
+        "floor", 
+        "year_built", 
+        "use", 
+        "plot_area", 
+        "living_space", 
+        "floor_space", 
+        "total_floor_area", 
+        "sia_gv_volume", 
+        "rent_squared_meters_month"
+    ]
+
+objects_headers = [
+    "contact_external_id", 
+    "portal_id", 
+    "vendor_id", 
+    "type_id", 
+    "detail_url", 
+    "title", 
+    "description", 
+    "street", 
+    "house_number", 
+    "zip_code", 
+    "city", 
+    "latitude", 
+    "longitude", 
+    "price", 
+    "living_space_area", 
+    "land_area", 
+    "rs_category_id", 
+    "price_value", 
+    "advertiser_id"
+]
+
+contacts_headers = [
+    "external_id", 
+    "first_name", 
+    "last_name", 
+    "organization_name", 
+    "email", 
+    "phone", 
+    "street", 
+    "house_number", 
+    "zip_code", 
+    "city", 
+    "normalized_phone", 
+    "portal_id", 
+    "vendor_id"
+]
+
+contacts_used_ids_headers = [
+    "id", 
+    "first_name", 
+    "last_name"
+]
